3f6876f71ced7160c350dcd8a60df842eeaec369
tiberiusocea <soceatiberiuv@gmail.com>
2020-02-02 12:41:43 +0200

    Updates

---
+++ precache-manifest.e8e7a92058d3e2ec2c5d5d392aa844a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff03693030edf6925bf42f6dc9dc813e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dc2fae9c689e37197fe0",
+    "url": "/monsters-rolodex/static/css/main.5bf80300.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2fae9c689e37197fe0",
+    "url": "/monsters-rolodex/static/js/main.38a0cadc.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
